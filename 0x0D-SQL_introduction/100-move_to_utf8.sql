730d6fe5aa513bac16752c88fe3d03ca460badf1
root <root@DESKTOP-SIEPB5L>
2024-03-13 09:53:55 +0200

    collation

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,14 @@
 -- go to utf
+-- change db
 USE hbtn_0c_0;
 
--- db
+-- change db character set and collation
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- change table character set and collation
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- table
+-- change column character set and collation
 ALTER TABLE first_table
 MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
