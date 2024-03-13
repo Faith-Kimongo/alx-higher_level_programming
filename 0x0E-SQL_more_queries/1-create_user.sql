b9cd0090beed68d575b7dd4a2444e3f51d0d8d48
root <root@DESKTOP-SIEPB5L>
2024-03-13 22:39:52 +0200

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- create mysql user
+DROP USER IF EXISTS user_0d_1@localhost;
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.*
+TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
