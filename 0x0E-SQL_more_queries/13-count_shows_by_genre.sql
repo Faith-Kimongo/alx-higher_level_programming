fa59b8afa30717aa332face1fc28abbf2a23b52c
Faith-Kimongo <faithvictor78@gmail.com>
2024-03-14 22:08:39 +0200

    shows grouped by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- shows linked
+SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_show_genres GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;
+
