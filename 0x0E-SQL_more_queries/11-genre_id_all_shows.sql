f99e4dd6a338d1ec5a972afcde0e780b63069a97
Faith-Kimongo <faithvictor78@gmail.com>
2024-03-14 21:49:10 +0200

    genre id for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- genre id for all shows
+SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
