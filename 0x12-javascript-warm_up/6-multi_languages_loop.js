0c2668426327b63c978b9d809ae0d45004a00138
Faith-Kimongo <faithvictor78@gmail.com>
2024-04-11 14:10:34 +0200

    loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// lloop languages to print three lines for each
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < words.length; i++) {
+  console.log(words[i]);
+}
