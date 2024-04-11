f4c8d64f6c4374c1b774e0f97eff978654c22f8e
Faith-Kimongo <faithvictor78@gmail.com>
2024-04-11 14:39:25 +0200

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// compute and print a factorial
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  function factorial (a) {
+    if (a === 0) {
+      return 1;
+    }
+    return a * factorial(a - 1);
+  }
+  console.log(factorial(num));
+}
