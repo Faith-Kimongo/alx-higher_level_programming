b4bb3a3bf05fec599b03726976ef82c1f8e7aa17
Faith-Kimongo <faithvictor78@gmail.com>
2024-04-12 12:41:07 +0200

    factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // import an array and compute a new array
-
 const list = require('./100-data.js').list;
-
-const newList = list.map((value, index) => value * index);
+const newList = list.map((value, index) => value * index); // Adjusting index to start from 1
 console.log(list);
 console.log(newList);
