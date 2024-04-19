15f331ab90eb9309ad14f66049544e7043a8877a
Faith-Kimongo <faithvictor78@gmail.com>
2024-04-19 20:26:53 +0200

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+Square class inherits from Rectangle class
+get the Rectangle class
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    initiate the Square class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+        get size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        set the size
+        """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """
+        string representation
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Update Rectangle"""
+        variables = ['id', 'size', 'x', 'y']
+        if args:
+            for index, value in enumerate(args):
+                if index < 4:
+                    setattr(self, variables[index], value)
+                else:
+                    break
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key in variables:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ return a dictionatry """
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
