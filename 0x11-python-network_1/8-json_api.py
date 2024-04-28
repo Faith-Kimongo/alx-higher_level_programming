b881177be9b524d002af7e4e8fca1a7f6e8fa8a1
Faith-Kimongo <faithvictor78@gmail.com>
2024-04-28 19:38:07 +0200

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+import required modules: requests, sys
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    """
+    only execute when run as main
+    script to get json data depending on the passed value
+    using post method
+    """
+    argv = sys.argv
+    latter = ""
+    if len(argv) == 2:
+        latter = argv[1]
+    values = {'q': latter}
+    url = 'http://0.0.0.0:5000/search_user'
+    resp = requests.post(url, data=values)
+    jsonData = resp.json()
+    if len(jsonData) > 0:
+        id = jsonData.get('id')
+        name = jsonData.get('name')
+        print(f"[{id}] {name}")
+    elif len(jsonData) == 0:
+        print("No result")
+    else:
+        print("Not a valid JSON")
